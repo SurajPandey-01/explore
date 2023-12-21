7d24fb2503f1e17557ec735738424aefa06ef1cc
SurajPandey-01 <145469219+SurajPandey-01@users.noreply.github.com>
2023-12-21 19:31:45 +0530

    index.js

--- init/index.js
+++ init/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const initData = require("./data.js");
 const List = require("../models/listing.js");
 
-const MONGO_URL = "mongodb://0.0.0.0:27017/wanderlust";
+const MONGO_URL = "mongodb+srv://surajpandey879122:C5rKml9Cou2YBMxf@cluster1.24elnsz.mongodb.net/?retryWrites=true&w=majority";
 
 main()
   .then(() => {
