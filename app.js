c17e9437a25f0da6319d6a63bc534e88712dd76d
SurajPandey-01 <145469219+SurajPandey-01@users.noreply.github.com>
2023-12-21 10:42:00 +0530

    app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const initData = require("./data.js");
-const List = require("../models/listing.js");
+const List = require("./models/listing.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
