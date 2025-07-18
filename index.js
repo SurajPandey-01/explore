7a4f090f8483e7096c4a9b60306554dd0d2615eb
SurajPandey-01 <145469219+SurajPandey-01@users.noreply.github.com>
2025-07-18 17:05:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 8080;
 const path = require("path");
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "/views"));
+app.set("views", path.join(__dirname, "/docs"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 const mongoose = require("mongoose");
